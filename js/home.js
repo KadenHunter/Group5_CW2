c5095ede016408df659b280246be49673c3bd2af
Kaden Hunter <x9gxq4h55g@privaterelay.appleid.com>
2026-03-28 20:30:10 +0000

    Began Homepage
    Began creating the homepage html, css and js features. Also have added global styles to styles.css which include background colours and the css for the nav bar and footer

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
 // Homepage JavaScript
+$(document).ready(function () {
+ 
+    /* Updates the copyright year in the footer automatically */
+    $('#year').text(new Date().getFullYear());
+ 
+    /* Toggles the mobile nav menu open and closed */
+    $('#hamburger').on('click', function () {
+        $('#nav-links').toggleClass('open');
+    });
+ 
+    /* Closes the menu when a nav link is clicked on mobile */
+    $('.nav-link').on('click', function () {
+        $('#nav-links').removeClass('open');
+    });
+ 
+});
+ 
