c1092223dae1300755a8b0c43d642b7557d5e9a3
Kaden Hunter <x9gxq4h55g@privaterelay.appleid.com>
2026-03-30 03:10:10 +0100

    Nav bar across all pages added code, linked each page to their specific css and js code, added a banner to the homepage
    I have added the Nav bar code to each persons web page as well as linking the correct css and js files to ensure each person has the correct file paths they need, as well as this I went in depth to fix nav bar positioning and added a banner image for the homepage ensuring it blends in well with the overall vibe of the website.

--- js/home.js
+++ js/home.js
@@ -4,15 +4,16 @@ $(document).ready(function () {
     /* Updates the copyright year in the footer automatically */
     $('#year').text(new Date().getFullYear());
  
+ 
     /* Toggles the mobile nav menu open and closed */
-    $('#hamburger').on('click', function () {
+    $('#hamburger').click(function () {
         $('#nav-links').toggleClass('open');
     });
  
+ 
     /* Closes the menu when a nav link is clicked on mobile */
-    $('.nav-link').on('click', function () {
+    $('.nav-link').click(function () {
         $('#nav-links').removeClass('open');
     });
  
 });
- 
