93bcee61dde48cf40cb149b81d899ec9193e6c1f
njan03 <nabbyjan@gmail.com>
2026-04-04 22:20:59 +0100

    finished entire staff section

--- js/staff.js
+++ js/staff.js
@@ -1,1 +1,9 @@
-// staff page JavaScript
+// Toggle staff bios
+$(".bio-btn").click(function () {
+  $(this).next(".bio").slideToggle();
+});
+
+// Hiring button interaction
+$("#apply-btn").click(function () {
+  $("#apply-message").fadeToggle();
+});
