8a2e800412a143de7ec167d2a2e6391aba13aaeb
Kaden Hunter <x9gxq4h55g@privaterelay.appleid.com>
2026-04-07 02:51:40 +0100

    Cleaned staff page to fit mark scheme requirements
    Cleaned staff page to fit accessibility  requirements and more js features

--- js/staff.js
+++ js/staff.js
@@ -1,9 +1,55 @@
-// Toggle staff bios
-$(".bio-btn").click(function () {
-  $(this).next(".bio").slideToggle();
-});
+// Staff page JavaScript
+$(document).ready(function () {
 
-// Hiring button interaction
-$("#apply-btn").click(function () {
-  $("#apply-message").fadeToggle();
+    // set the year in the footer automatically
+    $('#year').text(new Date().getFullYear());
+
+    // fade the page in when it loads
+    $('body').css('opacity', 0).animate({ opacity: 1 }, 800);
+
+    // open and close the mobile menu when the hamburger is clicked
+    $('#hamburger').click(function () {
+        $('#nav-links').toggleClass('open');
+    });
+
+    // close menu if a link is clicked (only applies to mobile)
+    $('.nav-link').click(function () {
+        $('#nav-links').removeClass('open');
+    });
+
+    // show and hide the bio when the button is clicked 
+    $('.bio-btn').click(function () {
+        $(this).next('.bio').slideToggle();
+        if ($(this).attr('aria-expanded') === 'true') {
+            $(this).attr('aria-expanded', 'false');
+        } else {
+            $(this).attr('aria-expanded', 'true');
+        }
+    });
+
+    // show the email when apply is clicked 
+    $('#apply-btn').click(function () {
+        $('#apply-message').fadeToggle();
+    });
+
+    // dim nav links on hover 
+    $('.nav-link').hover(
+        function () {
+            $(this).stop().animate({ opacity: 0.6 }, 150);
+        },
+        function () {
+            $(this).stop().animate({ opacity: 1 }, 150);
+        }
+    );
+
+    // dim social icons on hover 
+    $('.social-icon').hover(
+        function () {
+            $(this).stop().animate({ opacity: 0.6 }, 150);
+        },
+        function () {
+            $(this).stop().animate({ opacity: 1 }, 150);
+        }
+    );
+
 });
