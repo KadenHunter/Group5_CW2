61258f900a3b53c0f9a88f30e5d75b99add03433
emma123838 <barr-e14@ulster.ac.uk>
2026-04-08 13:23:17 +0100

    about us changes

--- js/about.js
+++ js/about.js
@@ -1,1 +1,36 @@
-// about page JavaScript
+$(document).ready(function () {
+
+    // footer year
+    $('#year').text(new Date().getFullYear());
+
+    // fade page in
+    $('body').css('opacity', 0).animate({ opacity: 1 }, 800);
+
+    // mobile menu
+    $('#hamburger').click(function () {
+        $('#nav-links').toggleClass('open');
+    });
+
+    $('.nav-link').click(function () {
+        $('#nav-links').removeClass('open');
+    });
+
+    // fade sections in
+    var delay = 200;
+    $('section').each(function () {
+        $(this).css('opacity', 0).delay(delay).animate({ opacity: 1 }, 600);
+        delay += 200;
+    });
+
+    // image hover fade
+    $('.welcome-img img, .app-img img').hover(
+        function () {
+            $(this).stop().animate({ opacity: 0.85 }, 200);
+        },
+        function () {
+            $(this).stop().animate({ opacity: 1 }, 200);
+        }
+    );
+
+});
+        
